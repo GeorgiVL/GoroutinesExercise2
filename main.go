ece2d1b0f726fd28f6faddc5d4ec3af4feceb0c2
CORP\Georgi.Valkanov <georgi.valkanov@acronis.com>
2022-07-19 16:14:41 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type human interface {
+	speak()
+}
+
+type person struct {
+	First  string
+	Second string
+	Age    int
+}
+
+func (p *person) speak() {
+	fmt.Printf("%s\t%s is speaking right now", p.First, p.Second)
+}
+
+func saySomething(h human) {
+	h.speak()
+}
+
+func main() {
+	p1 := person{
+		First:  "Georgi",
+		Second: "Valkanov",
+		Age:    24,
+	}
+
+	saySomething(&p1)
+
+}
